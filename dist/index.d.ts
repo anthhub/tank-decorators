6555444fc7ed41a40abcf0acf910f06f87322e7c
anthhub <anthhub@163.com>
2019-05-26 14:22:30 +0800

    fix dir

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    _get<T>(target: T, fn: (target: T) => any, defaultValue?: any): any;
-    _wait(time?: number): Promise<{}>;
-    _throttle<T extends () => any>(fn: T, delay: number): T;
-    _debounce<T extends () => any>(fn: T, delay: number): T;
-    _getPropsFromURL(name: string): string | null;
-    _timeToLocalStr(time: number): string;
-};
-export default _default;
--- dist/url/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description 从url获取参数
- * @author anthhub
- * @date 2019-05-26
- * @export
- * @param {string} name
- * @returns {string}
- */
-export declare function _getPropsFromURL(name: string): string | null;
